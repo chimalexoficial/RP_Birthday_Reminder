055802013da2a2d9f084ad6621eb642473ae31ac
chimalexoficial <you@example.com>
2022-10-26 19:37:05 -0700

    Project finished :)

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+
+  // my initial state is my data, initializing that here
+  const [people, setPeople] = useState(data);
+
+  return <main>
+    <section className='container'>
+      <h3>{people.length}</h3>
+      <List people={people} />
+      {/* passing an empty array here!! not 0 */}
+      <button onClick={() => setPeople([])}>Clear All</button>
+    </section>
+  </main>;
 }
 
 export default App;
